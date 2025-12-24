319be759da2512c4665ffbc803adcbaebf8474dc
maowimim <maowa087@gmail.com>
2025-12-25 02:00:58 +0600

    add footer in home page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Banner from '../Pages/Banner';
 import FeaturedSection from './FeaturedSection';
 import ContactUs from './ContactUs';
+import Footer from './Footer';
 
 const Home = () => {
     return (
@@ -9,6 +10,7 @@ const Home = () => {
           <Banner></Banner>
           <FeaturedSection></FeaturedSection>
           <ContactUs></ContactUs>
+          <Footer></Footer>
         </div>
     );
 };
